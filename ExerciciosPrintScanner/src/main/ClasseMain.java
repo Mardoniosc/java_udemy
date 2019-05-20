90bac25583ff1bcb7d2e15a12012a3ab8eecc687
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-20 20:53:58 -0300

    feito os exercicios

--- ExerciciosPrintScanner/src/main/ClasseMain.java
+++ ExerciciosPrintScanner/src/main/ClasseMain.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.util.Scanner;
+
 public class ClasseMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("----------------------------------");
+		System.out.println("Calculo da Área do Retângulo");
+		System.out.println("----------------------------------");
+		System.out.println("Digite o lado A do retângulo: ");
+		float ladoA = entrada.nextFloat();
+		System.out.println("----------------------------------");
+		System.out.println("Digite o lado B do retângulo: ");
+		float ladoB = entrada.nextFloat();
+		System.out.println("----------------------------------");
+		float area = ladoA * ladoB;
+		System.out.println("A área do retângulo é: " + area);
+		
+		
 	}
 
 }
