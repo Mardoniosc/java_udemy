068e6ec89fdc4ec56c231fe01932884943ca42ec
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-20 17:53:42 -0300

    alteção na classe principal

--- ProjetoScanner/src/main/Main.java
+++ ProjetoScanner/src/main/Main.java
@@ -26,6 +26,7 @@ public class Main {
 //		System.out.println("O numero desejado foi: " + numeroOpcao);
 		System.out.printf("O numero desejado foi %d \n", numeroOpcao);
 		System.out.println("-------------------------------------");
+		System.out.println("-------------------------------------");
 		
 	}
 
