3fc0f512de8e68e12eea621abf6b76830bd6ed54
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-20 21:35:20 -0300

    Atualizado projeto tipos de variaveis

--- ExerciciosPrintScanner/src/main/ClasseMain.java
+++ ExerciciosPrintScanner/src/main/ClasseMain.java
@@ -7,19 +7,23 @@ public class ClasseMain {
 	public static void main(String[] args) {
 
 		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("----------------------------------");
+		System.out.println("------------------------------------------");
 		System.out.println("Calculo da Área do Retângulo");
-		System.out.println("----------------------------------");
-		System.out.println("Digite o lado A do retângulo: ");
+		System.out.println("------------------------------------------");
+		System.out.println("Digite o lado A do retângulo em cm: ");
 		float ladoA = entrada.nextFloat();
 		System.out.println("----------------------------------");
-		System.out.println("Digite o lado B do retângulo: ");
+		System.out.println("Digite o lado B do retângulo em cm: ");
 		float ladoB = entrada.nextFloat();
-		System.out.println("----------------------------------");
+		System.out.println("-------------------------------------------");
 		float area = ladoA * ladoB;
-		System.out.println("A área do retângulo é: " + area);
-		
+		System.out.println("A área do retângulo é: " + area + "cm");
+		System.out.println("-------------------------------------------");
+		System.out.println("Digite a profundidade do retângulo em cm");
+		float profundidade = entrada.nextFloat();
+		float volume = area * profundidade;
+		System.out.println("-------------------------------------------");
+		System.out.println("O volume do retângulo é " + volume + "cm³");
 		
 	}
 
