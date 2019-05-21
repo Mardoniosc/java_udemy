ce87e936939026facc7ba4b652f6b2aa0ce1af8d
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-21 20:45:26 -0300

    adicionando o projeto arrays multidimensional

---
+++ ProjetoArraysMultidimensionais/src/projetoarraysmultidimensionais/ProjetoArraysMultidimensionais.java
@@ -0,0 +1,20 @@
+package projetoarraysmultidimensionais;
+
+public class ProjetoArraysMultidimensionais {
+
+	public static void main(String[] args) {
+
+		// declaração de array multidimensional
+		String cadastro[][] =
+			{
+					{"carlos","4444-4444"},
+					{"Fernando","9999-9999"},
+					{"Fabio","7777-7777"},
+					
+			};
+		System.out.println(cadastro.length);
+		System.out.println(cadastro[2][0]);
+		
+	}
+
+}
