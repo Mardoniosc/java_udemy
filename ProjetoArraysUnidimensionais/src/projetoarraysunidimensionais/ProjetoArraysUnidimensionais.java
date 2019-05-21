31e168c0e68c148cc621a77b5ad5cdc8f07ce7e8
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-21 20:35:01 -0300

    Adicionado o projeto array unidimensional

---
+++ ProjetoArraysUnidimensionais/src/projetoarraysunidimensionais/ProjetoArraysUnidimensionais.java
@@ -0,0 +1,30 @@
+package projetoarraysunidimensionais;
+
+public class ProjetoArraysUnidimensionais {
+
+	public static void main(String[] args) {
+
+		//array unidimensional
+		//declarar array
+		//Meses do ano
+		String [] mes = new String[12];
+		mes[0] = "Janeiro";
+		mes[1] = "Fevereiro";
+		mes[2] = "Março";
+		mes[3] = "Abril";
+		mes[4] = "Maio";
+		mes[5] = "Junho";
+		mes[6] = "Julho";
+		mes[7] = "Agosto";
+		mes[8] = "Setembro";
+		mes[9] = "Outubro";
+		mes[10] = "Novembro";
+		mes[11] = "Dezembro";
+	
+		System.out.println(mes[7]);
+		
+		System.out.println(mes.length);
+		
+	}
+
+}
