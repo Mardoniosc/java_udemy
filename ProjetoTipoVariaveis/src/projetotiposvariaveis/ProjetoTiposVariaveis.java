31e1da03a1109ebe0b7bab22846b61871b5a22d0
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-20 21:36:17 -0300

    adicionado o projeto tipo de variaveis

---
+++ ProjetoTipoVariaveis/src/projetotiposvariaveis/ProjetoTiposVariaveis.java
@@ -0,0 +1,42 @@
+package projetotiposvariaveis;
+
+public class ProjetoTiposVariaveis {
+
+	public static void main(String[] args) {
+		
+		//tipos de variaveis primitivas
+		
+		//tipos inteiros
+		
+		int numero1 = 1234;
+		System.out.println(numero1);
+		short numero2 = 4523;
+		byte numero3 = 127;
+		long numero4 = 6546542;
+		
+		//tipo caracter
+		char valorchar = 'a';
+		System.out.println(valorchar);
+		
+		//tipos flutuantes
+		float numero5 = 4.123f;
+		double numero6 = 45.656585;
+		System.out.println(numero5);
+		
+		
+		//tipo booleano = verdadeiro ou falso
+		boolean verdadeiro = true;
+		boolean falso = false;
+		System.out.println(verdadeiro);
+		
+		
+		//Tipos de variaveis não primitivas - são objetos
+		String frase = "Curso Java";
+		Float numero7 = 4.52f;
+		Double numero8 = 4.52;
+		
+		System.out.println(numero8 + numero8);
+		
+	}
+
+}
