e8a728df3f1133051411e0e8d092257b6a60625f
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-23 22:48:25 -0300

    Projeto do while exemplo

---
+++ ProjetoDoWhile/src/projetodowhile/ProjetoDoWhile.java
@@ -0,0 +1,18 @@
+package projetodowhile;
+
+public class ProjetoDoWhile {
+
+	public static void main(String[] args) {
+		
+		//Estrutura de repetição do while
+		
+		int contador = 0;
+		
+		do {
+			System.out.println("Repetição de numero: " + contador);
+			contador++;
+		} while (contador <=20);
+		
+	}
+
+}
