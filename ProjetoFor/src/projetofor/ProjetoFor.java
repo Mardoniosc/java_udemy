d61da3dc1f6f70ab77bbd4d0ce094be4dafbd830
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-05-23 23:05:55 -0300

    Feito projeto exemplo do for incrementando e decrementando

---
+++ ProjetoFor/src/projetofor/ProjetoFor.java
@@ -0,0 +1,22 @@
+package projetofor;
+
+public class ProjetoFor {
+
+	public static void main(String[] args) {
+
+		// estrutura for
+		
+		// incrementando
+		
+		for (int i=0;i<10;i++) {
+			System.out.println("Repetição de numero: " + i);
+		}
+		
+		
+		// decrementando
+		
+		for (int j=10;j>0;j--)
+			System.out.println("Falta " + j + " para finalizar o processo");
+	}
+
+}
