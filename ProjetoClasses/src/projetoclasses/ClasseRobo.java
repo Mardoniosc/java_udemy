dc6386ffa49d46c7294f206c11de89e53a85680b
MARDONIO SILVA DA COSTA <mardonio@live.com>
2019-05-25 16:07:43 -0300

    feito a classe robo

---
+++ ProjetoClasses/src/projetoclasses/ClasseRobo.java
@@ -0,0 +1,34 @@
+package projetoclasses;
+
+public class ClasseRobo {
+	
+	// atributos
+	public boolean ligadoDesligado = false;
+	public String nome;
+	public String altura;
+	public float velocidade;
+	public float aceleracao;
+
+	//metodos
+	public void ligarRobo() {
+		
+	}
+	public void desligarRobo() {
+		
+	}
+	public void andarParaFrente() {
+		
+	}
+	public void andarParaTras() {
+		
+	}
+	public void virarParaDireita() {
+		
+	}
+	public void virarParaEsquerda() {
+		
+	}
+	public void frear() {
+		
+	}
+}
