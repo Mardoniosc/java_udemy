d2f0cef416723d3da44b066027e627babda57840
MARDONIO SILVA DA COSTA <mardonio@live.com>
2019-05-25 14:40:21 -0300

    criado um projeto de passagem de valores diferentes tipos

---
+++ ProejetoPassagemValorDiferentesTipos/src/projetopassagemvalordiferentestipos/ProejetoPassagemValorDiferentesTipos.java
@@ -0,0 +1,16 @@
+package projetopassagemvalordiferentestipos;
+
+public class ProejetoPassagemValorDiferentesTipos {
+
+	public static void main(String[] args) {
+
+			MensagemIdade(28, "Parabens Mardonio pelos seus: ");
+		
+	}
+	
+	public static void MensagemIdade(int idade, String mensagem) {
+		
+		System.out.println(mensagem + idade + " Anos ");
+	}
+
+}
