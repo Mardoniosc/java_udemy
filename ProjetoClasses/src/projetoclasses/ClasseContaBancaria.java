7558c0f0bc8f6bf9a9c2bbd3160af0c6482ab468
MARDONIO SILVA DA COSTA <mardonio@live.com>
2019-05-25 16:23:12 -0300

    atualizado o pacotes classes criando a classe conta bancaria e produto

---
+++ ProjetoClasses/src/projetoclasses/ClasseContaBancaria.java
@@ -0,0 +1,30 @@
+package projetoclasses;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class ClasseContaBancaria {
+	
+	//atributos
+	public String numeroConta;
+	public int numeroAgencia;
+	public int codigoClient;
+	public BigDecimal saldoConta;
+	public Date dataAberturaConta;
+	
+	//metodos
+	public void dataAbertura() {
+		
+	}
+	public void sacar() {
+		
+	}
+	public void depositar() {
+		
+	}
+	public void saldoConta() {
+		
+	}
+	
+
+}
---
+++ ProjetoClasses/src/projetoclasses/ClasseProduto.java
@@ -0,0 +1,22 @@
+package projetoclasses;
+
+import java.math.BigDecimal;
+
+public class ClasseProduto {
+
+	//metodos
+	public int codigo;
+	public String nome;
+	public BigDecimal preco;
+	public String[] ingredientes;
+	
+	public void inserirProduto() {
+		
+	}
+	public void deletarProduto() {
+		
+	}
+	public void alterarProduto() {
+		
+	}
+}
