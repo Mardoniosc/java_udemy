72a6e78caeb0d6ecc127b948179b4816065165cf
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-06-21 21:15:38 -0300

    pequena atualização nos atributos nas classes do mini projeto

--- ProjetoMiniPOO/src/principal/DadosAdicionais.java
+++ ProjetoMiniPOO/src/principal/DadosAdicionais.java
@@ -2,4 +2,12 @@ package principal;
 
 public class DadosAdicionais extends Endereco{
 
+	// atributos
+	private String telefoneResidencial;
+	private String telefoneComercial;
+	private String celularPessoal;
+	private String celularComercial;
+	private boolean possuiWatzapp;
+	private boolean possuiWatzappComercial;
+	
 }
