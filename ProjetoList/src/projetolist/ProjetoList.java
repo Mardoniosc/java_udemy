fe17e8a67e543d535e9cddd34d222281213d1504
Mardonio Silva da Costa <p772920@corp.caixa.gov.br>
2019-07-01 21:44:18 -0300

    criado projeto exemplo sobre List bem parecido com o ArrayList

---
+++ ProjetoList/src/projetolist/ProjetoList.java
@@ -0,0 +1,22 @@
+package projetolist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjetoList {
+
+	public static void main(String[] args) {
+
+		Double area1 = 4.56;
+		Double area2 = 2.57;
+		
+		List<Double> areas = new ArrayList<>();
+		
+		areas.add(area1);
+		areas.add(area2);
+		
+		System.out.println(areas);
+		
+	}
+
+}
