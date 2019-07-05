b23771a1ef253323a032b911f123da6a4ced0a30
Mardonio Costa <mardonio@live.com>
2019-07-04 22:42:14 -0300

    concluido o primeiro projeto

--- Swing/ProjetoSwingHellowWorld/src/com/br/helloworld/TelaPrincipal.java
+++ Swing/ProjetoSwingHellowWorld/src/com/br/helloworld/TelaPrincipal.java
@@ -7,6 +7,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Font;
 
 public class TelaPrincipal extends JFrame {
 
@@ -32,15 +38,30 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
+		setTitle("Pagina Principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 487, 337);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JLabel lblHelloWorld = new JLabel("Hello World");
-		contentPane.add(lblHelloWorld, BorderLayout.CENTER);
+		JLabel lblHellowWorld = new JLabel("Hellow World!!");
+		lblHellowWorld.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(174)
+					.addComponent(lblHellowWorld)
+					.addContainerGap(217, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(116)
+					.addComponent(lblHellowWorld)
+					.addContainerGap(158, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
