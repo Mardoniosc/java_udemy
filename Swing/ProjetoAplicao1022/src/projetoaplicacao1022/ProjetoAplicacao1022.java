044c5d56202bd7964990a0fef1bdad2b3405cba8
Mardonio Costa <mardonio@live.com>
2019-07-08 23:26:04 -0300

    feito mais projetos exemplos de swing com eventos do mouse

---
+++ Swing/ProjetoAplicao1022/src/projetoaplicacao1022/ProjetoAplicacao1022.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoaplicacao1022;
+
+/**
+ *
+ * @author p772920
+ */
+public class ProjetoAplicacao1022 {
+    
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+                
+                System.out.println("Que merda vei");
+	}
+    
+    
+}
