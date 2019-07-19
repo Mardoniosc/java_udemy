c234da01c7b361d4430c80fe2fc283767d45e3b6
Mardonio Costa <mardonio@live.com>
2019-07-18 22:29:02 -0300

    Feito a atualização do ultimo projeto MDIFluido

--- Projetos/ProjetoBaseLayoutMDIFluido/src/projetobaselayoutmdi/FXMLTelaPrincipalController.java
+++ Projetos/ProjetoBaseLayoutMDIFluido/src/projetobaselayoutmdi/FXMLTelaPrincipalController.java
@@ -10,9 +10,12 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class FXMLTelaPrincipalController implements Initializable {
 
@@ -28,6 +31,8 @@ public class FXMLTelaPrincipalController implements Initializable {
     private Button btnMDI2;
     @FXML
     private AnchorPane apMenuEsquerdo;
+    @FXML
+    private Button btnChamarFXMLExterno;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -40,6 +45,11 @@ public class FXMLTelaPrincipalController implements Initializable {
                 try {
                     // Colocando no objeto a o FxmlMDI01 
                     AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("FXMLMDI01.fxml"));
+                    //Deixar o AncorPane FXMLMDI01
+                    AnchorPane.setTopAnchor(a, 0.0);
+                    AnchorPane.setLeftAnchor(a, 0.0);
+                    AnchorPane.setRightAnchor(a, 0.0);
+                    AnchorPane.setBottomAnchor(a, 0.0);
                     
                     // chamar dentro do apMenuEsquerdo FXMLAPmenuesquerdo
                     apMenuEsquerdo.getChildren().setAll(a);
@@ -57,9 +67,37 @@ public class FXMLTelaPrincipalController implements Initializable {
                 try {
                     // Colocando no objeto a o FxmlMDI01 
                     AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("FXMLMDI02.fxml"));
+                    //Deixar o AncorPane FXMLMDI02 FLUIDO ou ancorado em todos os lados
+                    AnchorPane.setTopAnchor(a, 0.0);
+                    AnchorPane.setLeftAnchor(a, 0.0);
+                    AnchorPane.setRightAnchor(a, 0.0);
+                    AnchorPane.setBottomAnchor(a, 0.0);
                     
                     // chamar dentro do apMenuEsquerdo FXMLAPmenuesquerdo
                     apMenuEsquerdo.getChildren().setAll(a);
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLTelaPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        
+        //evento para o botão acompanhar a largura da ApMenuDireito
+        btnMDI1.prefWidthProperty().bind(apMenuDireito.widthProperty());
+        //evento para o botão acompanhar a largura da ApMenuDireito
+        btnMDI2.prefWidthProperty().bind(apMenuDireito.widthProperty());
+        
+        
+        //foi criada a ação do click no botão
+        btnChamarFXMLExterno.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Stage stage = new Stage();
+                    Parent root = FXMLLoader.load(getClass().getResource("FXMLExterno.fxml"));
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
                     
                 } catch (IOException ex) {
                     Logger.getLogger(FXMLTelaPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
