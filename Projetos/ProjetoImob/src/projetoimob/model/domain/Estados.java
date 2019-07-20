8a95478ee7ed0d8d21b5102bad1d69651397a129
Mardonio Costa <mardonio@live.com>
2019-07-19 22:16:08 -0300

    Feito a criacao de todas as classes bases para o projeto e criado as classes de conexao com banco de dados postgrelsql e Mysql

---
+++ Projetos/ProjetoImob/src/projetoimob/model/domain/Estados.java
@@ -0,0 +1,31 @@
+
+package projetoimob.model.domain;
+
+/**
+ *
+ * @author fschi
+ */
+public class Estados {
+   
+    //atributos
+    private int id_Estados;
+    private String nome_Estado;
+    
+    //get e set
+    public int getId_Estados() {
+        return id_Estados;
+    }
+
+    public void setId_Estados(int id_Estados) {
+        this.id_Estados = id_Estados;
+    }
+
+    public String getNome_Estado() {
+        return nome_Estado;
+    }
+
+    public void setNome_Estado(String nome_Estado) {
+        this.nome_Estado = nome_Estado;
+    }
+    
+}
---
+++ Projetos/ProjetoImob/src/projetoimob/model/domain/LoginSistema.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoimob.model.domain;
+
+/**
+ *
+ * @author p772920
+ */
+public class LoginSistema {
+    
+}
